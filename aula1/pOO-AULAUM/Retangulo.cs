b1d2e9ead80b62e1cc39b8ecc4d49ca869618ad9
20211011110019 <20211011110019@CNAT377136>
2022-10-24 17:16:48 -0300

    ..

---
+++ aula1/pOO-AULAUM/Retangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pOO_AULAUM
+{
+    class Retangulo
+    {
+        private double b, h;
+        public void SetBase(double v)
+        {
+            if (v > 0) b = v;
+        }
+        public void SetAltura(double v)
+        {
+            if (v > 0) h = v;
+        }
+        public double GetBase()
+        {
+            return b;
+        }
+        public double GetAltura()
+        {
+            return h;
+        }
+        public double CalcArea()
+        {
+            return b * h;
+        }
+        public double CalcDiagonal()
+        {
+            return Math.Sqrt(b * b + h * h);
+        }
+
+    }
+}
